d94b4691a44c523bbf0e738c916fd6fb9d0aab63
seanl26 <seanl26@uw.edu>
2025-10-19 03:29:45 -0700

    mostly completed questionscreen

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignInScreen from '../screens/LoginAndRegisterScreen';
 import CoupleGateScreen from '../screens/CoupleGateScreen';
 import HomeScreen from '../screens/HomeScreen';
-import SplashScreen from "../screens/SplashScreen";
+import SplashScreen from '../screens/SplashScreen';
+import QuestionScreen from '../screens/QuestionScreen';
 
 const CreateCouplePlaceholder = () => (
   <></>
@@ -14,26 +15,27 @@ const JoinCouplePlaceholder = () => (
 );
 
 export type RootStackParamList = {
-    SplashScreen: undefined;
+  SplashScreen: undefined;
   SignIn: undefined;
   Home: undefined;
   CoupleGate: undefined;
   CreateCouple: undefined;
   JoinCouple: undefined;
+  QuestionScreen: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
-    <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="QuestionScreen" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="SignIn" component={SignInScreen} />
-        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+      <Stack.Screen name="SplashScreen" component={SplashScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="CoupleGate" component={CoupleGateScreen} />
       <Stack.Screen name="CreateCouple" component={CreateCouplePlaceholder} />
       <Stack.Screen name="JoinCouple" component={JoinCouplePlaceholder} />
-      <Stack.Screen name="DailyQuestion" component={QuestionScreen} />
+      <Stack.Screen name="QuestionScreen" component={QuestionScreen} />
     </Stack.Navigator>
   );
 }

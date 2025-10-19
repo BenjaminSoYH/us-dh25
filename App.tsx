fdc9fc1fd0165d1d4e8b1888db3a7eeffa790513
Orlando Scheil <orlandos.ks05@gmail.com>
2025-10-19 07:34:27 -0700

    Refactor App, Login, and SignUp screens for improved readability and functionality. Updated font loading logic in App.tsx, enhanced error handling and user feedback in LoginScreen.tsx, and implemented sign-up functionality with email verification in SignUpScreen.tsx.

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
-import {useFonts} from "expo-font";
+import { useFonts } from "expo-font";
 import * as ExpoSplashScreen from 'expo-splash-screen';
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-        'AbhayaLibre-ExtraBold': require('./assets/fonts/AbhayaLibre-ExtraBold.ttf'),
-    });
+  const [fontsLoaded] = useFonts({
+    'AbhayaLibre-ExtraBold': require('./assets/fonts/AbhayaLibre-ExtraBold.ttf'),
+  });
 
-    useEffect(() => {
-        async function prepare() {
-            if (fontsLoaded) {
-                await ExpoSplashScreen.hideAsync();
-            }
-        }
-        prepare();
-    }, [fontsLoaded]);
-
-    if (!fontsLoaded) {
-        return null;
+  useEffect(() => {
+    async function prepare() {
+      if (fontsLoaded) {
+        await ExpoSplashScreen.hideAsync();
+      }
     }
+    prepare();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
     <NavigationContainer>
